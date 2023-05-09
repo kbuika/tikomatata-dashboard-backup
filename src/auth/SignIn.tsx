e772a3c2a0b86b599585f048bc350eb3b0286278
kibuikaCodes <kibuikasteve@gmail.com>
2023-05-09 22:40:53 +0300

    add vercel config and test text colors

--- src/auth/SignIn.tsx
+++ src/auth/SignIn.tsx
@@ -5,17 +5,17 @@ export default function SignIn() {
     <div className='bg-gradient-to-r from-criticalBg to-successBg h-screen w-screen flex items-start justify-center lg:p-[100px] pt-[100px]'>
       <div className='h-100 w-[85%] lg:w-1/3 flex flex-col items-center'>
         <div>
-          <h2 className='text-[30px] font-normal'>Hey, Welcome Back</h2>
+          <h2 className='text-[30px] font-normal text-neutralDark'>Hey, Welcome Back</h2>
         </div>
-        <div className='w-full mt-[36px]'>
+        <div className='w-full mt-[36px] text-neutralDark'>
           <div>
-            <label>Email Address</label>
+            <label className='text-neutralDark'>Email Address</label>
             <input
               id='email'
               name='email'
               type='email'
               required
-              className='h-[50px] appearance-none rounded-sm relative block w-full px-3 py-2 border border-gray-600 placeholder-gray-500 text-gray-900 focus:border-none focus:outline-none focus:ring-2 focus:z-10 sm:text-sm'
+              className='h-[50px] bg-white appearance-none rounded-sm relative block w-full px-3 py-2 border border-gray-600 placeholder-gray-500 text-gray-900 focus:border-none focus:outline-none focus:ring-2 focus:z-10 sm:text-sm'
               placeholder='Email Address'
               autoComplete='nope'
             ></input>
@@ -23,13 +23,13 @@ export default function SignIn() {
         </div>
         <div className='w-full mt-[24px]'>
           <div>
-            <label>Password</label>
+            <label className='text-neutralDark'>Password</label>
             <input
               id='password'
               name='password'
               type='password'
               required
-              className='h-[50px] appearance-none rounded-sm relative block w-full px-3 py-2 border border-gray-600 placeholder-gray-500 text-gray-900 focus:border-none focus:outline-none focus:ring-2 focus:z-10 sm:text-sm'
+              className='h-[50px] bg-white appearance-none rounded-sm relative block w-full px-3 py-2 border border-gray-600 placeholder-gray-500 text-gray-900 focus:border-none focus:outline-none focus:ring-2 focus:z-10 sm:text-sm'
               placeholder='Enter Password'
               autoComplete='nope'
             ></input>
@@ -46,13 +46,16 @@ export default function SignIn() {
           </button>
         </div>
         <div className='w-full flex justify-center mt-[24px]'>
-          <p>
-            Don&rsquo;t have an account? <a href='/register'>Sign Up</a>
+          <p className='text-neutralDark'>
+            Don&rsquo;t have an account?{" "}
+            <a href='/register' className='text-secondary'>
+              Sign Up
+            </a>
           </p>
         </div>
         <div className='mt-[20px] mb-[20px]'>Or</div>
         <div className='w-full flex justify-center'>
-          <button className='h-[50px] w-full border border-neutralDark rounded-sm bg-white flex justify-center items-center focus:border-none focus:outline-none focus:ring-2'>
+          <button className='h-[50px] w-full text-neutralDark border border-neutralDark rounded-sm bg-white flex justify-center items-center focus:border-none focus:outline-none focus:ring-2'>
             <span className='mr-[10px]'>
               <img src={GoogleIcon} height={20} width={20}></img>
             </span>
