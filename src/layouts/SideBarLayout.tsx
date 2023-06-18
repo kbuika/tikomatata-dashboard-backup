cfd9dfa635dbc28ed4be7a46a7b51d561d13c8ae
kibuikaCodes <kibuikasteve@gmail.com>
2023-06-18 14:51:01 +0300

    fix double renders :bug:

--- src/layouts/SideBarLayout.tsx
+++ src/layouts/SideBarLayout.tsx
@@ -1,13 +1,9 @@
-import { Outlet } from "react-router-dom"
 import SideBar from "./SideBar"
 
 export default function SideBarLayout() {
   return (
     <div>
       <SideBar />
-      <div>
-        <Outlet />
-      </div>
     </div>
   )
 }
