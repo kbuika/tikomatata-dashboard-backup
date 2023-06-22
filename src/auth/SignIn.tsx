be8311253b3c6336958f8a8e78cc5641cced1ec5
kibuikaCodes <kibuikasteve@gmail.com>
2023-06-22 13:44:01 +0300

    add loaders for google auth buttons

--- src/auth/SignIn.tsx
+++ src/auth/SignIn.tsx
@@ -3,6 +3,9 @@ import { yupResolver } from "@hookform/resolvers/yup"
 import * as yup from "yup"
 import GoogleIcon from "../assets/icons/google.png"
 import { GOOGLE_AUTH_URL } from "../constants"
+import { Link } from "react-router-dom"
+import { useState } from "react"
+import { Loader2 } from "lucide-react"
 
 interface ISignInProps {
   setToken: (userToken: string) => void
@@ -17,6 +20,7 @@ type IFormInput = yup.InferType<typeof schema>
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function SignIn({ setToken }: ISignInProps) {
+  const [isGoogleLogin, setIsGoogleLogin] = useState(false)
   const {
     register,
     handleSubmit,
@@ -24,76 +28,89 @@ export default function SignIn({ setToken }: ISignInProps) {
   } = useForm<IFormInput>({ resolver: yupResolver(schema) })
   const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data)
 
+  const handleGoogleLogin = () => {
+    setIsGoogleLogin(true)
+  }
+
   return (
-    <div className='bg-gradient-to-r from-criticalBg to-successBg h-screen w-screen flex items-start justify-center lg:p-[100px] pt-[100px]'>
-      <div className='h-100 w-[85%] lg:w-1/3 flex flex-col items-center'>
+    <div className="bg-gradient-to-r from-criticalBg to-successBg h-screen w-screen flex items-start justify-center lg:p-[100px] pt-[100px]">
+      <div className="h-100 w-[85%] lg:w-1/3 flex flex-col items-center">
         <div>
-          <h2 className='text-[30px] font-normal text-neutralDark'>Hey, Welcome Back</h2>
+          <h2 className="text-[30px] font-normal text-neutralDark">Hey, Welcome Back</h2>
         </div>
-        <div className='w-full mt-[36px] text-neutralDark'>
+        <div className="w-full mt-[36px] text-neutralDark">
           <div>
-            <label className='text-neutralDark'>Email Address</label>
+            <label className="text-neutralDark">Email Address</label>
             <input
-              id='email'
-              type='email'
+              id="email"
+              type="email"
               required
-              className='h-[50px] bg-white appearance-none rounded-sm relative block w-full px-3 py-2 border border-gray-600 placeholder-gray-500 text-gray-900 focus:border-none focus:outline-none focus:ring-2 focus:z-10 sm:text-sm'
-              placeholder='Email Address'
-              autoComplete='nope'
+              className="h-[50px] bg-white appearance-none rounded-sm relative block w-full px-3 py-2 border border-gray-600 placeholder-gray-500 text-gray-900 focus:border-none focus:outline-none focus:ring-2 focus:z-10 sm:text-sm"
+              placeholder="Email Address"
+              autoComplete="nope"
               {...register("email", { required: true })}
             ></input>
-            {errors.email && <span className='text-criticalRed'>{errors.email?.message}</span>}
+            {errors.email && <span className="text-criticalRed">{errors.email?.message}</span>}
           </div>
         </div>
-        <div className='w-full mt-[24px]'>
+        <div className="w-full mt-[24px]">
           <div>
-            <label className='text-neutralDark'>Password</label>
+            <label className="text-neutralDark">Password</label>
             <input
-              id='password'
-              type='password'
+              id="password"
+              type="password"
               required
-              className='h-[50px] bg-white appearance-none rounded-sm relative block w-full px-3 py-2 border border-gray-600 placeholder-gray-500 text-gray-900 focus:border-none focus:outline-none focus:ring-2 focus:z-10 sm:text-sm'
-              placeholder='Enter Password'
-              autoComplete='nope'
+              className="h-[50px] bg-white appearance-none rounded-sm relative block w-full px-3 py-2 border border-gray-600 placeholder-gray-500 text-gray-900 focus:border-none focus:outline-none focus:ring-2 focus:z-10 sm:text-sm"
+              placeholder="Enter Password"
+              autoComplete="nope"
               {...register("password", { required: true })}
             ></input>
             {errors.password && (
-              <span className='text-criticalRed'>{errors.password?.message}</span>
+              <span className="text-criticalRed">{errors.password?.message}</span>
             )}
           </div>
         </div>
-        <div className='w-full flex justify-end mt-[20px]'>
-          <a className='text-sm text-mainSecondary' href='/forgot-password'>
+        <div className="w-full flex justify-end mt-[20px]">
+          <a className="text-sm text-mainSecondary" href="/forgot-password">
             Forgot password?
           </a>
         </div>
-        <div className='w-full mt-[20px]'>
+        <div className="w-full mt-[20px]">
           <button
-            type='submit'
+            type="submit"
             onClick={handleSubmit(onSubmit)}
-            className='h-[50px] group relative w-full flex justify-center items-center py-2 px-4 border border-gray-600 text-sm font-medium rounded-sm text-black bg-mainPrimary focus:outline-none focus:ring-2 focus:ring-offset-2'
+            className="h-[50px] group relative w-full flex justify-center items-center py-2 px-4 border border-gray-600 text-sm font-medium rounded-sm text-black bg-mainPrimary focus:outline-none focus:ring-2 focus:ring-offset-2"
           >
             Continue
           </button>
         </div>
-        <div className='w-full flex justify-center mt-[24px]'>
-          <p className='text-neutralDark'>
+        <div className="w-full flex justify-center mt-[24px]">
+          <p className="text-neutralDark">
             Don&rsquo;t have an account?{" "}
-            <a href='/register' className='text-mainSecondary'>
+            <a href="/register" className="text-mainSecondary">
               Sign Up
             </a>
           </p>
         </div>
-        <div className='mt-[20px] mb-[20px] text-neutralDark'>Or</div>
-        <div className='w-full flex justify-center'>
-          <a href={GOOGLE_AUTH_URL} className='w-full'>
-            <button className='h-[50px] w-full text-neutralDark border border-neutralDark rounded-sm bg-white flex justify-center items-center focus:border-none focus:outline-none focus:ring-2'>
-              <span className='mr-[10px]'>
+        <div className="mt-[20px] mb-[20px] text-neutralDark">Or</div>
+        <div className="w-full flex justify-center">
+          <Link to={GOOGLE_AUTH_URL} className="w-full">
+            <button
+              onClick={handleGoogleLogin}
+              className="h-[50px] w-full text-neutralDark border border-neutralDark rounded-sm bg-white flex justify-center items-center focus:border-none focus:outline-none focus:ring-2"
+            >
+              <span className="mr-[10px]">
                 <img src={GoogleIcon} height={20} width={20}></img>
               </span>
-              Continue with Google
+              {isGoogleLogin ? (
+                <>
+                  <Loader2 className="mr-2 h4 w-4 animate-spin" /> Setting you up
+                </>
+              ) : (
+                "Continue with Google"
+              )}
             </button>
-          </a>
+          </Link>
         </div>
       </div>
     </div>
--- src/auth/SignUp.tsx
+++ src/auth/SignUp.tsx
@@ -8,7 +8,7 @@ import { registerUser } from "../apiCalls"
 import { checkRegistrationError, errorToast, formatPhoneNumber, successToast } from "../lib/utils"
 import { useState } from "react"
 import { Loader2 } from "lucide-react"
-import { useNavigate } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
 interface ISignUpProps {
   setToken: (userToken: string) => void
@@ -35,6 +35,7 @@ export default function SignUp({ setToken }: ISignUpProps) {
   const [loading, setLoading] = useState<boolean>(false)
   const [regError, setRegError] = useState<any>(null)
   const [, setErrorMessage] = useState<string>("")
+  const [isGoogleLogin, setIsGoogleLogin] = useState<boolean>(false)
 
   const navigate = useNavigate()
 
@@ -67,152 +68,165 @@ export default function SignUp({ setToken }: ISignUpProps) {
     }
   }
 
+  const handleGoogleLogin = () => {
+    setIsGoogleLogin(true)
+  }
+
   return (
-    <div className='bg-gradient-to-r from-criticalBg to-successBg h-auto flex items-start justify-center lg:p-[100px] py-[100px]'>
-      <div className='h-auto w-[90%] lg:w-1/3 flex flex-col items-center'>
+    <div className="bg-gradient-to-r from-criticalBg to-successBg h-auto flex items-start justify-center lg:p-[100px] py-[100px]">
+      <div className="h-auto w-[90%] lg:w-1/3 flex flex-col items-center">
         <div>
-          <h2 className='text-[30px] font-normal text-neutralDark'>Hey, lets get started!</h2>
+          <h2 className="text-[30px] font-normal text-neutralDark">Hey, lets get started!</h2>
         </div>
-        <div className='w-full mt-[36px]'>
+        <div className="w-full mt-[16px]">
           <div>
-            <label className='text-neutralDark'>Full Name</label>
+            <label className="text-neutralDark">Full Name</label>
             <input
-              id='name'
-              type='text'
+              id="name"
+              type="text"
               required
-              className='h-[50px] bg-white appearance-none rounded-sm relative block w-full px-3 py-2 border border-gray-600 placeholder-gray-500 text-gray-900 focus:border-none focus:outline-none focus:ring-2 focus:z-10 sm:text-sm'
-              placeholder='Enter Full Name'
-              autoComplete='nope'
+              className="h-[50px] bg-white appearance-none rounded-sm relative block w-full px-3 py-2 border border-gray-600 placeholder-gray-500 text-gray-900 focus:border-none focus:outline-none focus:ring-2 focus:z-10 sm:text-sm"
+              placeholder="Enter Full Name"
+              autoComplete="nope"
               {...register("fullName", { required: true })}
             ></input>
             {errors.fullName && (
-              <span className='text-criticalRed'>{errors.fullName?.message}</span>
+              <span className="text-criticalRed">{errors.fullName?.message}</span>
             )}
             {checkRegistrationError("fullName", regError)?.hasError && (
-              <span className='text-criticalRed'>
+              <span className="text-criticalRed">
                 {checkRegistrationError("fullName", regError)?.message}
               </span>
             )}
           </div>
         </div>
-        <div className='w-full mt-[16px]'>
+        <div className="w-full mt-[16px]">
           <div>
-            <label className='text-neutralDark'>Phone Number</label>
-            <div className='flex items-center'>
-              <span className='w-[35%] text-neutralDark lg:w-1/4 bg-white h-[50px] flex items-center justify-center rounded-sm border border-hidden-left border-gray-600'>
-                <img src={KenyaIcon} alt='Kenyan Flag' className='mr-2' />
+            <label className="text-neutralDark">Phone Number</label>
+            <div className="flex items-center">
+              <span className="w-[35%] text-neutralDark lg:w-1/4 bg-white h-[50px] flex items-center justify-center rounded-sm border border-hidden-left border-gray-600">
+                <img src={KenyaIcon} alt="Kenyan Flag" className="mr-2" />
                 +254
               </span>
               <input
-                id='phone'
-                type='text'
+                id="phone"
+                type="text"
                 required
-                className='w-3/4 h-[50px] bg-white appearance-none rounded-sm relative block w-full px-3 py-2 border border-gray-600 placeholder-gray-500 text-gray-900 focus:border-none focus:outline-none focus:ring-2 focus:z-10 sm:text-sm'
-                placeholder='Phone number'
-                autoComplete='nope'
+                className="w-3/4 h-[50px] bg-white appearance-none rounded-sm relative block w-full px-3 py-2 border border-gray-600 placeholder-gray-500 text-gray-900 focus:border-none focus:outline-none focus:ring-2 focus:z-10 sm:text-sm"
+                placeholder="Phone number"
+                autoComplete="nope"
                 {...register("phone", { required: true })}
               ></input>
             </div>
           </div>
-          {errors.phone && <span className='text-criticalRed'>{errors.phone?.message}</span>}
+          {errors.phone && <span className="text-criticalRed">{errors.phone?.message}</span>}
           {checkRegistrationError("phone", regError)?.hasError && (
-            <span className='text-criticalRed'>
+            <span className="text-criticalRed">
               {checkRegistrationError("phone", regError)?.message}
             </span>
           )}
         </div>
-        <div className='w-full mt-[16px]'>
+        <div className="w-full mt-[16px]">
           <div>
-            <label className='text-neutralDark'>Email Address</label>
+            <label className="text-neutralDark">Email Address</label>
             <input
-              id='email'
-              type='email'
+              id="email"
+              type="email"
               required
-              className='h-[50px] bg-white appearance-none rounded-sm relative block w-full px-3 py-2 border border-gray-600 placeholder-gray-500 text-gray-900 focus:border-none focus:outline-none focus:ring-2 focus:z-10 sm:text-sm'
-              placeholder='Email Address'
-              autoComplete='nope'
+              className="h-[50px] bg-white appearance-none rounded-sm relative block w-full px-3 py-2 border border-gray-600 placeholder-gray-500 text-gray-900 focus:border-none focus:outline-none focus:ring-2 focus:z-10 sm:text-sm"
+              placeholder="Email Address"
+              autoComplete="nope"
               {...register("email", { required: true })}
             ></input>
-            {errors.email && <span className='text-criticalRed'>{errors.email?.message}</span>}
+            {errors.email && <span className="text-criticalRed">{errors.email?.message}</span>}
             {checkRegistrationError("email", regError)?.hasError && (
-              <span className='text-criticalRed'>
+              <span className="text-criticalRed">
                 {checkRegistrationError("email", regError)?.message}
               </span>
             )}
           </div>
         </div>
-        <div className='w-full mt-[16px]'>
+        <div className="w-full mt-[16px]">
           <div>
-            <label className='text-neutralDark'>Password</label>
+            <label className="text-neutralDark">Password</label>
             <input
-              id='password'
-              type='password'
+              id="password"
+              type="password"
               required
-              className='h-[50px] bg-white appearance-none rounded-sm relative block w-full px-3 py-2 border border-gray-600 placeholder-gray-500 text-gray-900 focus:border-none focus:outline-none focus:ring-2 focus:z-10 sm:text-sm'
-              placeholder='Enter Password'
-              autoComplete='nope'
+              className="h-[50px] bg-white appearance-none rounded-sm relative block w-full px-3 py-2 border border-gray-600 placeholder-gray-500 text-gray-900 focus:border-none focus:outline-none focus:ring-2 focus:z-10 sm:text-sm"
+              placeholder="Enter Password"
+              autoComplete="nope"
               {...register("password", { required: true })}
             ></input>
             {errors.password && (
-              <span className='text-criticalRed'>{errors.password?.message}</span>
+              <span className="text-criticalRed">{errors.password?.message}</span>
             )}
             {checkRegistrationError("password", regError)?.hasError && (
-              <span className='text-criticalRed'>
+              <span className="text-criticalRed">
                 {checkRegistrationError("password", regError)?.message}
               </span>
             )}
           </div>
         </div>
-        <div className='w-full flex justify-start mt-[20px]'>
-          <div className='relative flex gap-x-3'>
-            <div className='flex h-6 items-center'>
+        <div className="w-full flex justify-start mt-[20px]">
+          <div className="relative flex gap-x-3">
+            <div className="flex h-6 items-center">
               <input
-                id='comments'
-                name='comments'
-                type='checkbox'
-                className='h-4 w-4 bg-white rounded border-gray-300 text-indigo-600 focus:ring-indigo-200'
+                id="comments"
+                name="comments"
+                type="checkbox"
+                className="h-4 w-4 bg-white rounded border-gray-300 text-indigo-600 focus:ring-indigo-200"
               ></input>
             </div>
-            <div className='text-sm leading-6'>
-              <label htmlFor='comments' className='font-medium text-gray-900'>
+            <div className="text-sm leading-6">
+              <label htmlFor="comments" className="font-medium text-gray-900">
                 Agree to terms & conditions
               </label>
             </div>
           </div>
         </div>
-        <div className='w-full mt-[20px]'>
+        <div className="w-full mt-[20px]">
           <button
-            type='submit'
+            type="submit"
             onClick={handleSubmit(onSubmit)}
-            className='h-[50px] group relative w-full flex justify-center items-center py-2 px-4 border border-gray-600 text-sm font-medium rounded-sm text-black bg-mainPrimary focus:outline-none focus:ring-2 focus:ring-offset-2'
+            className="h-[50px] group relative w-full flex justify-center items-center py-2 px-4 border border-gray-600 text-sm font-medium rounded-sm text-black bg-mainPrimary focus:outline-none focus:ring-2 focus:ring-offset-2"
           >
             {loading ? (
               <>
-                <Loader2 className='mr-2 h4 w-4 animate-spin' /> Setting you up
+                <Loader2 className="mr-2 h4 w-4 animate-spin" /> Setting you up
               </>
             ) : (
               "Continue"
             )}
           </button>
         </div>
-        <div className='w-full flex justify-center mt-[16px]'>
-          <p className='text-neutralDark'>
+        <div className="w-full flex justify-center mt-[16px]">
+          <p className="text-neutralDark">
             Already have an account?{" "}
-            <a href='/sign-in' className='text-mainSecondary'>
+            <a href="/sign-in" className="text-mainSecondary">
               Sign In
             </a>
           </p>
         </div>
-        <div className='mt-[20px] mb-[20px] text-neutralDark'>Or</div>
-        <div className='w-full flex justify-center'>
-          <a href={GOOGLE_AUTH_URL} className='w-full'>
-            <button className='h-[50px] text-neutralDark w-full border border-neutralDark rounded-sm bg-white flex justify-center items-center focus:border-none focus:outline-none focus:ring-2'>
-              <span className='mr-[10px]'>
+        <div className="mt-[20px] mb-[20px] text-neutralDark">Or</div>
+        <div className="w-full flex justify-center">
+          <Link to={GOOGLE_AUTH_URL} className="w-full">
+            <button
+              onClick={handleGoogleLogin}
+              className="h-[50px] w-full text-neutralDark border border-neutralDark rounded-sm bg-white flex justify-center items-center focus:border-none focus:outline-none focus:ring-2"
+            >
+              <span className="mr-[10px]">
                 <img src={GoogleIcon} height={20} width={20}></img>
               </span>
-              Continue with Google
+              {isGoogleLogin ? (
+                <>
+                  <Loader2 className="mr-2 h4 w-4 animate-spin" /> Setting you up
+                </>
+              ) : (
+                "Continue with Google"
+              )}
             </button>
-          </a>
+          </Link>
         </div>
       </div>
     </div>
