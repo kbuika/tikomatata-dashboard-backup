2b258a900190c198c6a9deff3f0e214eb0c4f78b
kibuikaCodes <kibuikasteve@gmail.com>
2023-06-25 15:53:53 +0300

    :sparkles: add modal to create collaborators

--- src/components/manage-event/EventCollaborators.tsx
+++ src/components/manage-event/EventCollaborators.tsx
@@ -9,6 +9,14 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "../ui/dialog"
+import {
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+} from "../ui/select"
 
 const EventCollaborators = () => {
   return (
@@ -16,7 +24,57 @@ const EventCollaborators = () => {
       <div className="text-neutralDark">
         <div className="w-full flex flex-row items-center justify-between">
           <h2 className="text-[23px] font-semibold">event Collaborators</h2>
-          <CustomButton className="">Add Collaborator</CustomButton>
+          <Dialog>
+            <DialogTrigger asChild>
+              <CustomButton className="mt-4">Add Collaborator</CustomButton>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+              <DialogHeader>
+                <DialogTitle>Add a Collaborator</DialogTitle>
+                <DialogDescription>
+                  Make sure the email is valid. Click save when you&apos;re done.
+                </DialogDescription>
+              </DialogHeader>
+              <div className="grid gap-4 py-4">
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <label htmlFor="name" className="text-right">
+                    Name
+                  </label>
+                  <Input id="name" placeholder="John Doe" className="col-span-3" />
+                </div>
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <label htmlFor="email" className="text-right">
+                    Email
+                  </label>
+                  <Input
+                    id="email"
+                    type="email"
+                    placeholder="johndoe@email.com"
+                    className="col-span-3"
+                  />
+                </div>
+                <div className="grid grid-cols-4 items-center gap-4 w-[100%]">
+                  <label htmlFor="role" className="text-right">
+                    Role
+                  </label>
+                  <Select name="role">
+                    <SelectTrigger className="col-span-3">
+                      <SelectValue placeholder="Select role for user" />
+                    </SelectTrigger>
+                    <SelectContent className="col-span-3">
+                      <SelectGroup>
+                        <SelectItem value="admin">Admin</SelectItem>
+                        <SelectItem value="viewer">Viewer</SelectItem>
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                </div>
+              </div>
+              <DialogFooter>
+                <CustomButton type="submit">Invite Collaborator</CustomButton>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
         </div>
       </div>
       <div className="h-auto w-auto flex flex-col items-start mt-[3em]">
@@ -53,6 +111,22 @@ const EventCollaborators = () => {
                     placeholder="johndoe@email.com"
                     className="col-span-3"
                   />
+                </div>
+                <div className="grid grid-cols-4 items-center gap-4 w-[100%]">
+                  <label htmlFor="role" className="text-right">
+                    Role
+                  </label>
+                  <Select name="role">
+                    <SelectTrigger className="col-span-3">
+                      <SelectValue placeholder="Select role for user" />
+                    </SelectTrigger>
+                    <SelectContent className="col-span-3">
+                      <SelectGroup>
+                        <SelectItem value="admin">Admin</SelectItem>
+                        <SelectItem value="viewer">Viewer</SelectItem>
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
                 </div>
               </div>
               <DialogFooter>
